7bb4752771c7fbca82ed5019c82b8c5190dead9f
tabo <kakha_tabagari@yahoo.com>
2013-03-09 21:21:42 +0400

    dual talent warrior priest sql

---
+++ sql/updates/globalwow/dual_talent_warrior_priest.sql
@@ -0,0 +1,3 @@
+UPDATE creature_template SET gossip_menu_id="0" WHERE entry ="914";
+UPDATE creature_template SET gossip_menu_id="0" WHERE entry ="5480";
+UPDATE creature_template SET gossip_menu_id="0" WHERE entry ="5489";
